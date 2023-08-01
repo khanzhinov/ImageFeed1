b2a601a8cab1c5d59310daebd09bb58e58dd1ae6
khanzhinov <khanzhinov@yandex.ru>
2023-08-01 19:24:42 +0300

    создан новый класс

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Валерия Медведева on 01.08.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton () {
+    }
+}
