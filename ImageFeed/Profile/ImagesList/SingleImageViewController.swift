dfcd9e1935420ce3bb1797c6772ea18c1dc274ba
khanzhinov <khanzhinov@yandex.ru>
2023-08-06 18:35:22 +0300

    добавлен SingleImageViewController

---
+++ ImageFeed/Profile/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Валерия Медведева on 06.08.2023.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+}
