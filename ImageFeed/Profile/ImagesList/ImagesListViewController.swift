a76d79bc33b58a139f2a6b1809062292b988027a
khanzhinov <khanzhinov@yandex.ru>
2023-08-07 22:19:29 +0300

    открывает картинку!!

--- ImageFeed/Profile/ImagesList/ImagesListViewController.swift
+++ ImageFeed/Profile/ImagesList/ImagesListViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Валерия Медведева on 29.07.2023.
 //
 
-import Foundation
+
 import UIKit
 
 class ImagesListViewController: UIViewController {
--- ImageFeed/Profile/ImagesList/SingleImageViewController.swift
+++ ImageFeed/Profile/ImagesList/SingleImageViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Валерия Медведева on 06.08.2023.
 //
 
-import Foundation
+
 import UIKit
 
 final class SingleImageViewController: UIViewController {
