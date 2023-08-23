93e5263bd9b0b8d7fa4de238dc07f82f7ff84649
khanzhinov <khanzhinov@yandex.ru>
2023-08-23 13:10:33 +0300

    добавлен новый файл AuthViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Валерия Медведева on 23.08.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
+
+
