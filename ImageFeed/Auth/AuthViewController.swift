054a9acb9474194b325106854f538a3ea8178e26
khanzhinov <khanzhinov@yandex.ru>
2023-08-28 18:54:46 +0300

    перед уроком URLComponents

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
 }
 
 
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Валерия Медведева on 23.08.2023.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        
+    }
+    
+}
+
+
