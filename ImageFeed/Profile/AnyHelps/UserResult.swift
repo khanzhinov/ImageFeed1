f6325ff6164b8a01a84ed27838d043f38855a960
ezielx <130781302+khanzhinov@users.noreply.github.com>
2023-09-06 10:09:26 +0300

    добавлен ProfileImageService, приложение не крашится

---
+++ ImageFeed/Profile/AnyHelps/UserResult.swift
@@ -0,0 +1,22 @@
+//
+//  UserResult.swift
+//  ImageFeed
+//
+//  Created by Валерия Медведева on 06.09.2023.
+//
+
+import Foundation
+
+struct UserResult: Codable {
+    let profileImage: ProfileImage?
+    
+    enum CodingKeys: String, CodingKey {
+        case profileImage = "profile_image"
+    }
+}
+
+struct ProfileImage: Codable {
+    let small: String?
+    let medium: String?
+    let large: String?
+}
