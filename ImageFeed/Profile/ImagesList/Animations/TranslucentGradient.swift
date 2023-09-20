28ce117a1a7ffb5e03b9145be72118d7131f14ba
ezielx <130781302+khanzhinov@users.noreply.github.com>
2023-09-20 20:27:18 +0300

    изменен алертмодел

---
+++ ImageFeed/Profile/ImagesList/Animations/TranslucentGradient.swift
@@ -0,0 +1,44 @@
+//
+//  TranslucentG.swift
+//  ImageFeed
+//
+//  Created by Валерия Медведева on 20.09.2023.
+//
+
+
+import QuartzCore
+import UIKit
+
+
+final class TranslucentGradient {
+    func getGradient(size: CGSize, cornerRadius: CGFloat = 9) -> CAGradientLayer{
+        let gradient = CAGradientLayer()
+
+        gradient.frame = CGRect(origin: .zero, size: size)
+        gradient.locations = [0, 0.1, 0.3]
+        gradient.colors = [
+            UIColor(red: 0.682, green: 0.686, blue: 0.706, alpha: 1).cgColor,
+            UIColor(red: 0.531, green: 0.533, blue: 0.553, alpha: 1).cgColor,
+            UIColor(red: 0.431, green: 0.433, blue: 0.453, alpha: 1).cgColor
+        ]
+        gradient.startPoint = CGPoint(x: 0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        gradient.cornerRadius = cornerRadius
+        gradient.masksToBounds = true
+
+        gradient.add(getAnimation(), forKey: "locationsChange")
+
+        return gradient
+    }
+
+    private func getAnimation() -> CABasicAnimation {
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "locations")
+        gradientChangeAnimation.duration = 1.0
+        gradientChangeAnimation.repeatCount = .infinity
+        gradientChangeAnimation.fromValue = [0, 0.1, 0.3]
+        gradientChangeAnimation.toValue = [0, 0.8, 1]
+
+        return gradientChangeAnimation
+    }
+}
+
