a8c3c6c382d92de15d95b8eeefad2557a74393dd
ezielx <130781302+khanzhinov@users.noreply.github.com>
2023-09-30 15:16:39 +0300

    приложение работает корректно, написан тест WebView

--- ProfileViewTest/ProfileViewPresenterSpy.swift
+++ ProfileViewTest/ProfileViewPresenterSpy.swift
@@ -13,20 +13,20 @@ import ImageFeed
 final class ProfileViewPresenterSpy: ProfileViewPresenterProtocol {
     var view: ImageFeed.ProfileViewControllerProtocol?
     
- 
-     var viewDidLoadCalled: Bool = false
-
-     func viewDidLoad() {
-         viewDidLoadCalled = true
-     }
-
-     func logOut() {
-
-     }
-
-     func updateAvatar() {
-
-     }
-
-
- }
+    
+    var viewDidLoadCalled: Bool = false
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func logOut() {
+        
+    }
+    
+    func updateAvatar() {
+        
+    }
+    
+    
+}
